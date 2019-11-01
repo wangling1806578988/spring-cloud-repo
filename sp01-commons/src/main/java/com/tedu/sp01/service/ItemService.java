8c6c6d7079c36aecc01c3c6803f9c3e4e62b7acc
wangling1806578988 <55574348+wangling1806578988@users.noreply.github.com>
2019-11-01 15:57:57 +0800

    Update ItemService.java

--- sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -3,7 +3,7 @@ package com.tedu.sp01.service;
 import java.util.List;
 
 import com.tedu.sp01.pojo.Item;
-
+//66
 public interface ItemService {
 	List<Item> getItems(String orderId);
 	void decreaseNumbers(List<Item> list);
